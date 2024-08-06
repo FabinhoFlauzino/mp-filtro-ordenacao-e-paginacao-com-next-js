dbc0174f47de66ab6e7a0daf9f9984711b1da9e3
unknown <fabioflauzino@hotmail.com>
2024-08-05 21:13:48 -0300

    Finalizando projeto

--- app/page.tsx
+++ app/page.tsx
@@ -13,22 +13,35 @@ import {
 import axios from 'axios';
 
 type ComponentProps = {
-  searchParams: {
-    search?: string
-    status?: string
-    sort?: string
-  }
-}
+  searchParams?: {
+    search?: string;
+    status?: string;
+    sort: string;
+    page: number;
+  };
+};
 
 export default async function Component({ searchParams }: ComponentProps) {
-  const response = await axios.get(`https://apis.codante.io/api/orders-api/orders`, {
-    params: {
-      search: searchParams?.search,
-      status: searchParams?.status,
-      sort: searchParams?.sort
+  const response = await axios.get(
+    'https://apis.codante.io/api/orders-api/orders',
+    {
+      params: {
+        search: searchParams?.search,
+        status: searchParams?.status,
+        sort: searchParams?.sort,
+        page: searchParams?.page,
+      },
     }
-  })
-  const orders = response.data.data
+  );
+
+  const orders = response.data.data;
+  const lastPage = response.data.meta.last_page;
+  let links: { url: string; label: string; active: boolean; id: number }[] =
+    response.data.meta.links;
+    console.log(links);
+    
+
+  links = links.map((link, index) => ({ ...link, id: index }));
 
   return (
     <main className="container px-1 py-10 md:p-10">
@@ -46,7 +59,7 @@ export default async function Component({ searchParams }: ComponentProps) {
         <CardContent>
           <OrdersTable orders={orders} />
           <div className="mt-8">
-            <Pagination />
+            <Pagination links={links} lastPage={lastPage} />
           </div>
         </CardContent>
       </Card>
